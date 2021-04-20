6ba5f9b267b580a83db4f4c48af5eaa4e52921e9
lisa <lisa.hetzel@jh-webworks.de>
2021-04-20 19:05:02 +0200

    implemented styles in frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <html>
+        <body>
+          <header>
+            <a href="/">
+              <img
+                src="/assets/uni_head_official-colorinverted.svg"
+                alt="Goethe-Universität Frankfurt am Main"
+              />
+            </a>
+            <div className="title">Kurzlink-Service</div>
+          </header>
+          <div id="main"></div>
+          <footer>
+            <section>
+              <a href="http://www.uni-frankfurt.de/">
+                Die Goethe-Universität Frankfurt am Main
+              </a>
+              <div className="pull-right">
+                <a href="/datenschutz">Datenschutzerklärung</a>|
+                <a href="/impressum">Impressum</a>|<a href="/AGB">AGB</a>
+              </div>
+            </section>
+          </footer>
+        </body>
+      </html>
     </div>
   );
 }
