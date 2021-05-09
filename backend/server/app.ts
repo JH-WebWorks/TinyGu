6f9637165a0053c4e723858b3ef97fc28a8e6a3e
Keiwan Jamaly <keiwanjamaly@gmail.com>
2021-05-09 19:15:04 +0200

    deleted old frondend parts

--- backend/server/app.ts
+++ backend/server/app.ts
@@ -5,13 +5,8 @@ import express from "express";
 const app = express();
 const port = process.env.PORT || "8080";
 
-app.set('port', port)
+app.set("port", port);
 
-import path from "path";
-// import favicon from 'serve-favicon';
-
-
-import index from "./routes/index";
 import redirect from "./routes/redirect";
 import create from "./routes/create";
 // const create = require("./routes/create");
@@ -26,29 +21,6 @@ app.use(compression());
 // app.use(helmet());
 
 app.use(express.json());
-
-// website routing
-app.use("/", index);
-app.use("/impressum", index);
-app.use("/404-Not-Found", index);
-app.use(
-  "/lib/jquery",
-  express.static(path.join(__dirname, "..", "node_modules/jquery/dist"))
-);
-
-// app.use(favicon(path.join(__dirname, '..', "public", "favicon.ico")));
-app.use(
-  "/styles",
-  express.static(path.join(__dirname, "..", "public", "styles"))
-);
-app.use(
-  "/assets",
-  express.static(path.join(__dirname, "..", "public", "assets"))
-);
-app.use(
-  "/pages",
-  express.static(path.join(__dirname, "..", "public", "pages"))
-);
 
 // api routing
 app.use("/api/create", create);
